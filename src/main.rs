109d675d50b67de080c5c48230e1c033a5e4c777
ricardo-medeiros_pismo <ricardo.medeiros@pismo.io>
2023-05-29 15:43:28 -0300

    servers: Minor improvements at servers
    This patch changes GET /start/{port} EP to the REST compliant PUT /servers/{port}. It also adds crossbeam-channels in orderto handle TCP servers without share global state between the threads.
    
    Signed-off-by: ricardo-medeiros_pismo <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
 #[macro_use]
 extern crate log;
 
-use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, put, web, App, HttpResponse, HttpServer, Responder};
+use crossbeam_channel::{unbounded, Sender};
 use env_logger::Env;
 use std::net::TcpListener;
 use std::thread;
 
+enum Action {
+    Start(TcpListener),
+}
+
+struct Command {
+    port: u16,
+    action: Action,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
+    let (tx, rx) = unbounded();
     info!("Bootstrapping the application");
 
-    HttpServer::new(|| {
+    thread::spawn(move || loop {
+        let command: Command = rx.recv().unwrap();
+        match command.action {
+            Action::Start(l) => {
+                let p = command.port;
+                thread::spawn(move || {
+                    for stream in l.incoming() {
+                        stream.unwrap();
+                        //let stream = stream.unwrap();
+
+                        //handle_connection(stream);
+                    }
+                });
+                // Add listening into servers vectors
+                info!("Server added at 127.0.0.1:{p}");
+            }
+        }
+    });
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(tx.clone()))
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
             .service(status)
@@ -28,8 +59,9 @@ async fn status() -> impl Responder {
     HttpResponse::Ok().body("OK")
 }
 
-#[get("/start/{port}")]
-async fn start(path: web::Path<String>) -> impl Responder {
+#[put("/servers/{port}")]
+async fn start(data: web::Data<Sender<Command>>, path: web::Path<u16>) -> impl Responder {
+    let tx = data;
     let port = path.into_inner();
     let addr = format!("127.0.0.1:{port}");
     info!("Starting TCP server at {addr}");
@@ -41,15 +73,11 @@ async fn start(path: web::Path<String>) -> impl Responder {
     }
 
     let listener = listener.unwrap();
-
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            stream.unwrap();
-            //let stream = stream.unwrap();
-
-            //handle_connection(stream);
-        }
-    });
+    tx.send(Command {
+        port: port,
+        action: Action::Start(listener),
+    })
+    .unwrap();
 
     HttpResponse::Ok().body("Server is running")
 }
